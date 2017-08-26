4e1c8e879eab4eed99530b755486c438ca21448e
Jakub Knejzlik <jakub.knejzlik@gmail.com>
2017-08-26 07:54:10 +0200

    Update scripts

--- test/fixtures.js
+++ test/fixtures.js
@@ -1,41 +1,57 @@
-'use strict'
+"use strict";
 
-const fs = require('fs')
-const mysql = require('mysql')
-const Promise = require('bluebird')
+const fs = require("fs");
+const mysql = require("mysql");
+const Promise = require("bluebird");
 
-let createArray = (n) => {
-    var data = [];
-    for(var i = 0; i < n; i++) {
-        data.push(i);
-    }
-    return data
-}
+let createArray = n => {
+  var data = [];
+  for (var i = 0; i < n; i++) {
+    data.push(i);
+  }
+  return data;
+};
 
 exports.prepare = () => {
-  let connection = Promise.promisifyAll(mysql.createConnection('mysql://root:test@localhost/test?multipleStatements=1'))
+  let connection = Promise.promisifyAll(
+    mysql.createConnection(
+      "mysql://root:test@localhost/test?multipleStatements=1"
+    )
+  );
 
-  let sql = fs.readFileSync(__dirname + '/fixtures.sql','utf-8')
-  return connection.queryAsync(sql).then(() => {
-    return Promise.map(createArray(1),(x) => {
-        // console.log('creating batch',x)
-        let values = []
+  let sql = fs.readFileSync(__dirname + "/fixtures.sql", "utf-8");
+  return connection
+    .queryAsync(sql)
+    .then(() => {
+      return Promise.map(
+        createArray(1),
+        x => {
+          // console.log('creating batch',x)
+          let values = [];
 
-        for (let i = 0; i < 1000; i++) {
-            values.push(`('user_${x}_${i}','firstname_${x}_${i}','lastname_${x}_${i}','1980-01-01' + INTERVAL ${i} DAY)`)
-        }
+          for (let i = 0; i < 1000; i++) {
+            values.push(
+              `('user_${x}_${i}','firstname_${x}_${i}','lastname_${x}_${i}','1980-01-01' + INTERVAL ${i} DAY)`
+            );
+          }
 
-        let insert = 'INSERT INTO `users` (`username`,`firstname`,`lastname`,`birthdate`) VALUES ' + values.join(',')
-        return connection.queryAsync(insert)
-    },{concurrency: 5})
-  }).then(() => {
-    let values = []
+          let insert =
+            "INSERT INTO `users` (`username`,`firstname`,`lastname`,`birthdate`) VALUES " +
+            values.join(",");
+          return connection.queryAsync(insert);
+        },
+        { concurrency: 5 }
+      );
+    })
+    .then(() => {
+      let values = [];
 
-    for (let i = 0; i < 10000; i++) {
-      values.push(`('my car ${i}','chevy ${i}')`)
-    }
+      for (let i = 0; i < 10000; i++) {
+        values.push(`('my car ${i}','chevy ${i}')`);
+      }
 
-    let insert = 'INSERT INTO `cars` (`name`,`brand`) VALUES ' + values.join(',')
-    return connection.queryAsync(insert)
-  })
-}
+      let insert =
+        "INSERT INTO `cars` (`name`,`brand`) VALUES " + values.join(",");
+      return connection.queryAsync(insert);
+    });
+};
--- test/index.test.js
+++ test/index.test.js
@@ -1,34 +1,33 @@
-'use strict'
+"use strict";
 
-const assert = require('assert')
-const mysql = require('mysql')
+const assert = require("assert");
+const mysql = require("mysql");
 
-const mysqlStreams = require('../lib/mysql')
-const mssqlStreams = require('../lib/mssql')
-const handler = require('../lib/query')
+const mysqlStreams = require("../lib/mysql");
+const mssqlStreams = require("../lib/mssql");
+const handler = require("../lib/query");
 
-const fixtures = require('./fixtures')
+const fixtures = require("./fixtures");
 
-describe('handler', () => {
+describe("handler", () => {
+  beforeEach(() => {
+    return fixtures.prepare();
+  });
 
-    beforeEach(() => {
-        return fixtures.prepare()
-    })
-
-    it('test', () => {
-      let sql = `SELECT
+  it("test", () => {
+    let sql = `SELECT
         id,
         username,
         firstname,
         lastname,
         birthdate
         FROM users
-      `
-      return handler({
-        connectionUrl: 'mysql://root:test@localhost/test',
-        sql: sql
-      }).then((result) => {
-        assert.equal(result.length, 1000)
-      })
-    })
-})
+      `;
+    return handler({
+      connectionUrl: "mysql://root:test@localhost/test",
+      sql: sql
+    }).then(result => {
+      assert.equal(result.length, 1000);
+    });
+  });
+});
